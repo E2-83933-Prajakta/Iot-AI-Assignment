28dc4620b2aea35c7650d2c2e22c4e3e962595c1
prajaktapujari123 <prajaktapujari93@gmail.com>
2024-05-02 22:42:06 +0530

    Iot -AI assing added

---
+++ assignment-1/q2.py
@@ -0,0 +1,10 @@
+# program to find avg of 3 int numbers
+
+num1 = int(input("Enter num1 : "))
+num2=int(input("Enter num2:"))
+num3=int(input("Enter num3:"))
+
+avg=(num1+num2+num3)/3
+
+print(f"avg of 3 numbers={avg}")
+
